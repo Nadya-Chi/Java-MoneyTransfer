4c476ce400743988973f90cbe6a0822a594b72fc
Nadya Chistyakova <npodtynchenko@list.ru>
2020-09-16 20:40:57 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int money = 500_000_000;
+        int total = balance + money;
+        System.out.println(total);
+    }
+}
